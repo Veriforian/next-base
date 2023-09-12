d13a66c767d7d059bab14d89ee447b7e6cdfb2e0
Ben Jones <benjones14480@gmail.com>
2023-09-11 23:24:47 -0600

    Begin CSS only nav

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -10,19 +10,27 @@ function NavLink({ href, text }) {
 
 function Navbar() {
   return (
-    <nav className="absolute lg:relative top flex items-center justify-around p-4 w-full text-lg">
-      <div className="flex-1 flex justify-around basis-2/6">
-        <NavLink href="/" text="Home" />
-        <NavLink href="/about" text="About" />
-      </div>
-      <div className="flex-1 basis-3/6 flex justify-center">
-        <a href="/">
-          <Logo className="fill-main-medium " />
-        </a>
-      </div>
-      <div className="flex-1 flex justify-around items-center basis-2/6">
-        <NavLink href="/contact" text="Contact" />
-        <BtnInv><a href="/#quote">Get Started</a></BtnInv>
+    <nav className="flex justify-between px-4 py-2 w-full">
+      <a className="" href="/">
+        <Logo className="fill-main-medium " />
+      </a>
+      {/* eslint-disable-next-line */}
+      <label htmlFor="burger" id="burger-label">Burger</label>
+      <input id="burger" type="checkbox" className="hidden" />
+      <div className="absolute lg:relative top flex items-center justify-around p-4 w-full text-lg items">
+        <div className="flex-1 flex justify-around basis-2/6">
+          <NavLink href="/" text="Home" />
+          <NavLink href="/about" text="About" />
+        </div>
+        <div className="flex-1 basis-3/6 flex justify-center">
+          <a href="/" className="hidden">
+            <Logo className="fill-main-medium " />
+          </a>
+        </div>
+        <div className="flex-1 flex justify-around items-center basis-2/6">
+          <NavLink href="/contact" text="Contact" />
+          <BtnInv><a href="/#quote">Get Started</a></BtnInv>
+        </div>
       </div>
     </nav>
   );
