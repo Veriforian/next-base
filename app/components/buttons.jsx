78024df9375059f833bcfbba946ff4b7b58ce11f
Ben Jones <benjones14480@gmail.com>
2023-09-28 17:26:38 -0600

    Continue hero separation

--- app/components/buttons.jsx
+++ app/components/buttons.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
 
-export function BtnSm({ children }) {
-  return <button type="button" className="btn btn-sm">{children}</button>;
+export function BtnSm({ children, className }) {
+  return <button type="button" className={`${className} bg-main-medium px-4 py-2 rounded-sm relative text-white-medium hover:text-main-medium hover:border-main-medium hover:border-solid hover:border-2 hover:bg-opacity-0 transition-all duration-75`}>{children}</button>;
 }
 
-export function BtnMed({ children }) {
-  return <button type="button" className="btn btn-sm">{children}</button>;
+export function BtnMed({ children, className }) {
+  return <button type="button" className={`${className}`}>{children}</button>;
 }
 
-export function BtnTxt({ children }) {
-  return <button type="button" className="btn btn-sm">{children}</button>;
+export function BtnTxt({ children, className }) {
+  return <button type="button" className={`${className} transition-all duration-75 `}>{children}</button>;
 }
 
-export function BtnInv({ children }) {
-  return <button type="button" className="border-2 border-main-medium rounded text-center px-3 py-2 flex items-center hover:bg-main-medium hover:text-white-medium transition-all delay-75 active:-translate-y-1 hover:ring-0">{children}</button>;
+export function BtnInv({ children, className }) {
+  return <button type="button" className={`${className} border-2 border-main-medium rounded text-center px-3 py-2 flex items-center hover:bg-main-medium hover:text-white-medium transition-all delay-75 active:-translate-y-1 hover:ring-0`}>{children}</button>;
 }
 
-export function BtnInvSm({ children }) {
-  return <button type="button" className="border-2 border-main-medium rounded-full text-center px-[25%] py-2 flex items-center hover:bg-main-medium hover:text-white-medium transition-all delay-75 active:-translate-y-1 hover:ring-0">{children}</button>;
+export function BtnInvSm({ children, className }) {
+  return <button type="button" className={`${className} border-2 border-main-medium rounded-full text-center px-[25%] py-2 flex items-center hover:bg-main-medium hover:text-white-medium transition-all active:-translate-y-1 hover:ring-0`}>{children}</button>;
 }
--- app/components/landing/hero.jsx
+++ app/components/landing/hero.jsx
@@ -1,24 +1,55 @@
 import React from 'react';
+import { PiArrowRightThin } from 'react-icons/pi';
+
+import './hero.css';
 import { BtnSm, BtnTxt } from '../buttons';
+
+function SectionLeftTop() {
+  return (
+    <div className="flex flex-col items-center justify-start h-[25vh] w-full">
+      <h1 className="text-5xl font-bold text-center text-main-light">
+        Welcome to the
+        {' '}
+        <span className=" text-dark-medium">landing</span>
+        {' '}
+        page
+      </h1>
+      <p className="text-xl pt-5 text-[1.2rem] w-[90%]">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <div className="mt-5 flex justify-center items-center ">
+        <BtnSm><a href="#quote">Get Started</a></BtnSm>
+        <div className=" w-10" />
+        <BtnTxt className="group hover:text-main-dark">
+          <a href="#wow" className="flex">
+            Why us
+            {' '}
+            <PiArrowRightThin className="text-xl translate-x-1 group-hover:translate-x-2 transition-all duration-[50ms]" />
+          </a>
+        </BtnTxt>
+
+      </div>
+    </div>
+  );
+}
+
+function SectionRightBotton() {
+  return (
+    <div className="hero-right flex flex-col items-center justify-end h-[25vh] w-full rounded-sm mt-10">
+      <h1 className="text-5xl font-bold text-center text-main-light">
+        Welcome to the
+        {' '}
+        <span className=" text-dark-medium">landing</span>
+        {' '}
+        page
+      </h1>
+    </div>
+  );
+}
 
 function Hero() {
   return (
     <section id="hero" className="mt-20 px-6 flex flex-col items-center">
-      <div className="flex flex-col items-center justify-start h-[25vh] w-full">
-        <h1 className="text-5xl font-bold text-center text-main-light">
-          Welcome to the
-          {' '}
-          <span className=" text-dark-medium">landing</span>
-          {' '}
-          page
-        </h1>
-        <p className="text-xl pt-5 text-[1.2rem] w-[90%]">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        <div className="">
-          <BtnSm>Get Started</BtnSm>
-          <BtnTxt>Why us</BtnTxt>
-        </div>
-      </div>
-      <figure />
+      <SectionLeftTop />
+      <SectionRightBotton />
     </section>
   );
 }
